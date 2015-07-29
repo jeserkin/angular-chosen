523524d1ce6a4ee5a00abe04f4d62c59577fe8a4
jeserkin <jeserkin@gmail.com>
2015-07-29 18:41:55 +0300

    feat(examples): add examples

--- example/app.js
+++ example/app.js
@@ -3,6 +3,40 @@
 angular.module('app', ['angular-chosen']);
 
 angular.module('app')
-  .controller('AngularCtrl', function() {
+  .controller('AngularCtrl', function($scope) {
+    // Base
+    $scope.options = {
+      selectedStaticOption: undefined,
+      selectedOptionFromHash: undefined,
+      selectedDisabledOption: undefined,
+      selectedMultipleOptions: undefined
+    };
 
+    // Hash
+    $scope.optionsAsHash = ['grooo', 'wowowowow', 'lakakalakakl', 'yadayada', 'insight', 'delve', 'synergy'];
+
+    // Disabling
+    var optionsForEmptyCollection = ['look', 'i', 'have', 'data'];
+    $scope.optionsForEmptyCollection = optionsForEmptyCollection;
+
+    $scope.isEmptyCollection = false;
+    $scope.toggleEmpty = function() {
+      $scope.isEmptyCollection = !$scope.isEmptyCollection;
+
+      if ($scope.isEmptyCollection) {
+        $scope.options.selectedDisabledOption = undefined;
+        $scope.optionsForEmptyCollection = [];
+      }
+      else {
+        $scope.optionsForEmptyCollection = optionsForEmptyCollection;
+      }
+    };
+
+    // External changing
+    $scope.optionsForExternalChange = {
+      cat: 'Cat',
+      dog: 'Dog',
+      hamster: 'Hamster'
+    };
+    $scope.options.selectedMultipleOptions = ['cat'];
   });
