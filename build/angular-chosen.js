d744518ea049a791d338c3b834c0f11b5d534acb
jeserkin <jeserkin@gmail.com>
2015-09-29 17:23:18 +0300

    feat(required): add support for required validator
    Fixed required attribute presence check

--- build/angular-chosen.js
+++ build/angular-chosen.js
@@ -1,5 +1,5 @@
 /**
- * angular-chosen 0.0.1-rc.3
+ * angular-chosen 0.0.1-rc.4
  * @author Eugene Serkin
  * @license MIT License http://opensource.org/licenses/MIT
  */
@@ -8,38 +8,37 @@
 (function(angular) {
     angular.module("angular-chosen", []);
     angular.module("angular-chosen").directive("chosen", function($parse, $timeout) {
-        var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
+        var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/, defaultText = "", _init = function(element, options) {
+            options = element.chosen(options).data("chosen");
+            element.trigger("chosen:updated");
+            defaultText = options.default_text;
+            element.addClass("angular-chosen");
+        }, _isEmpty = function(value) {
+            if (angular.isArray(value)) {
+                return value.length === 0;
+            } else if (angular.isObject(value)) {
+                var key;
+                for (key in value) {
+                    if (value.hasOwnProperty(key)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }, _disableStateMonitoring = function(element, attrs) {
+            attrs.$observe("disabled", function() {
+                return element.trigger("chosen:updated");
+            });
+        }, _enable = function(element) {
+            element.attr("data-placeholder", defaultText).removeAttr("disabled").trigger("chosen:updated");
+        }, _disable = function(element, options) {
+            element.attr("data-placeholder", options.results_none_found).attr("disabled", "disabled").trigger("chosen:updated");
+        };
         return {
             restrict: "A",
             require: "?ngModel",
             link: function(scope, iElement, iAttrs, ngModelCtrl) {
-                var _el = angular.element(iElement), options = $parse(iAttrs.chosen)() || {}, defaultText = "";
-                var _init = function(element) {
-                    options = element.chosen(options).data("chosen");
-                    element.trigger("chosen:updated");
-                    defaultText = options.default_text;
-                    _el.addClass("angular-chosen");
-                }, _isEmpty = function(value) {
-                    if (angular.isArray(value)) {
-                        return value.length === 0;
-                    } else if (angular.isObject(value)) {
-                        var key;
-                        for (key in value) {
-                            if (value.hasOwnProperty(key)) {
-                                return false;
-                            }
-                        }
-                    }
-                    return true;
-                }, _disableStateMonitoring = function(element) {
-                    iAttrs.$observe("disabled", function() {
-                        return element.trigger("chosen:updated");
-                    });
-                }, _enable = function(element) {
-                    element.attr("data-placeholder", defaultText).removeAttr("disabled").trigger("chosen:updated");
-                }, _disable = function(element) {
-                    element.attr("data-placeholder", options.results_none_found).attr("disabled", "disabled").trigger("chosen:updated");
-                }, _ngOptionsMonitoring = function(element) {
+                var _el = angular.element(iElement), options = $parse(iAttrs.chosen)() || {}, _ngOptionsMonitoring = function(element) {
                     if (!iAttrs.ngOptions || !ngModelCtrl) {
                         return;
                     }
@@ -49,7 +48,7 @@
                             if (angular.isUndefined(newVal)) {} else {
                                 _enable(element);
                                 if (_isEmpty(newVal)) {
-                                    _disable(element);
+                                    _disable(element, options);
                                 }
                             }
                         });
@@ -57,7 +56,7 @@
                 };
                 if (ngModelCtrl) {
                     ngModelCtrl.$formatters.push(function(modelValue) {
-                        _init(_el);
+                        _init(_el, options, defaultText);
                         return modelValue;
                     });
                     ngModelCtrl.$parsers.push(function(viewValue) {
@@ -67,14 +66,14 @@
                         return !value || value.length === 0;
                     };
                     ngModelCtrl.$validators.required = function(modelValue) {
-                        if (iAttrs.required === false) {
+                        if (angular.isUndefined(iAttrs.required) || iAttrs.required === false) {
                             return true;
                         }
                         return !ngModelCtrl.$isEmpty(modelValue);
                     };
                 }
                 _ngOptionsMonitoring(_el);
-                _disableStateMonitoring(_el);
+                _disableStateMonitoring(_el, iAttrs);
             }
         };
     });
